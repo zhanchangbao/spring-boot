ab796cac5979600ed2ca9dd88b7ead765c1b92d8
zhanchangbao <zhanchangbao@126.com>
2019-03-26 10:54:27 +0800

    initial commit

---
+++ springboot-02-autoconfig/src/main/java/com/example/Springboot02AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02AutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02AutoconfigApplication.class, args);
+    }
+
+}
